be6bb5ac87147680d67b9935d86ce50706846a93
Musab Kılıç <musabkilic@protonmail.com>
2024-12-03 14:11:08 +0100

    remove A, B variable declaration
    i decided it is much more fun to try and solve without declaring another variable(not counting function variables ofc)
    
    there is probably a much nicer way of unzipping and converting tuple to array but idk

--- 1.fs
+++ 1.fs
@@ -1,17 +1,29 @@
 ﻿open System
 open System.IO
 
-let lines =
-    File.ReadAllLines(@"1.txt")
-    |> Array.map (fun x -> x.Split("   ") |> Array.map (fun x -> x |> int))
+let DEBUG = false
 
-let A = lines |> Array.map (fun x -> x[0]) |> Array.sort
-let B = lines |> Array.map (fun x -> x[1]) |> Array.sort
-let ans1 = (A, B) ||> Array.map2 (fun a b -> abs (a - b)) |> Array.sum
+let dbg v =
+    if DEBUG then
+        printfn "%A" v
+
+    v
+
+let lines =
+    File.ReadAllLines(@"input")
+    |> Array.map (fun x -> x.Split("   ") |> Array.map (int) |> (fun x -> (x.[0], x.[1])))
+    |> Array.unzip
+    |> fun (a, b) -> [| a; b |]
+
+let ans1 =
+    lines
+    |> Array.map (Array.sort)
+    |> fun x -> (x.[0], x.[1]) ||> Array.map2 (fun a b -> abs (a - b)) |> Array.sum
 
 let ans2 =
-    A
-    |> Array.map (fun a -> a * (B |> Array.filter (fun x -> x = a) |> Array.length))
-    |> Array.sum
+    lines
+    |> Array.map (Array.sort)
+    |> fun x -> (x.[0], x.[1] |> Array.countBy (id) |> Map.ofArray)
+    ||> fun A B -> A |> Array.sumBy (fun a -> a * (B |> Map.tryFind a |> Option.defaultValue 0))
 
-printfn "%i %i" ans1 ans2
+printfn "%A %A" ans1 ans2
